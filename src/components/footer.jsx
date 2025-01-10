b95a4b8f0f132a5b2a251eac1becc73190f8be92
Damar <damar728@gmail.com>
2025-01-10 17:14:23 +0700

    first commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -53,7 +53,7 @@ function Footer() {
             </div>
             <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
             <div className="sm:flex sm:items-center sm:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center">© 2025 <Link to="/" className="hover:underline">Bungkus™ | PT. Asia Kreasi Solusi</Link>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center">© 2025 <Link to="/" className="hover:underline">Bungkus™ | CV. Asia Kreasi Solusi</Link>. All Rights Reserved.
                 </span>
             </div>
             </div>
