c3150a8599fd81c92394d459af44a7fdaaf4ad9c
Damar <damar728@gmail.com>
2025-01-15 01:20:03 +0700

    first commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
         <nav className="bg-cyan-200 border-gray-200">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="/logo.png" className="h-16" alt="Bungkus Logo" />
+                    <img src="/logo.png" className="h-24" alt="Bungkus Logo" />
                     <span className="hidden">Bungkus</span>
                 </Link>
                 <Button toggleMenu={toggleMenu} />
